130c7be36a365a668b108bf12a9e4245be90b782
Towphe <castillochristopherjames@gmail.com>
2025-01-11 22:42:17 +0800

    configured android application

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'gob-konek',
-  webDir: 'dist'
+  appId: "gob.konek.app",
+  appName: "gobkonek",
+  webDir: "public",
 };
 
 export default config;
