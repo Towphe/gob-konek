640f5a02788afb024a788202c931c47939eddaf1
Towphe <castillochristopherjames@gmail.com>
2025-01-12 02:12:39 +0800

    added language support, darkmode lightmode, and more

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,6 +7,7 @@ import {
   IonSelect,
   IonSelectOption,
   IonTitle,
+  IonToggle,
   IonToolbar,
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
@@ -14,17 +15,61 @@ import { useEffect, useState } from "react";
 import { Redirect } from "react-router";
 import { provinces, cities } from "../util/location";
 
+interface Translation {
+  English: string;
+  Filipino: string;
+  Cebuano: string;
+  Waray: string;
+  Hiligaynon: string;
+}
+
 const Index: React.FC = () => {
   const [topSearches, setTopSearches] = useState<string[]>([]);
   const [search, setSearch] = useState<string | undefined>();
   const [isGeolocationEnabled, toggleGeolocation] = useState<boolean>(false);
-  const [selectedProvince, setSelectedProvince] = useState<
-    string | undefined
-  >();
+  const [selectedProvince, setSelectedProvince] = useState<string | undefined>(
+    localStorage.getItem("province") ?? undefined,
+  );
   const [selectedMunicipalityOrCity, setSelectedMunicipalityOrProvince] =
-    useState<string | undefined>();
-  const [selectedLanguage, setSelectedLanguage] = useState<string>("English");
-  const languageOptions: string[] = ["English", "Filipino", "Cebuano"];
+    useState<string | undefined>(localStorage.getItem("city") ?? undefined);
+  const [selectedLanguage, setSelectedLanguage] = useState<string>(
+    localStorage.getItem("language") ?? "English",
+  );
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(
+    localStorage.getItem("isDarkMode") === "false" ? false : true,
+  );
+  const languageOptions: string[] = [
+    "English",
+    "Filipino",
+    "Cebuano",
+    "Waray",
+    "Hiligaynon",
+  ];
+  const popularSearchesTranslations: Translation = {
+    English: "Popular Searches",
+    Filipino: "Mga Patok Ngayon",
+    Cebuano: "Sikat Karon",
+    Waray: "Mga Sikat Yana",
+    Hiligaynon: "Mga Patok Subong",
+  };
+  const enterTextTranslations: Translation = {
+    English: "Enter person",
+    Filipino: "Mag-enter ng tao",
+    Cebuano: "Mag-enter ng tao",
+    Waray: "Mag-enter ng tao",
+    Hiligaynon: "Mag-enter ng tao",
+  };
+  const viewTranslations: Translation = {
+    English: "View",
+    Filipino: "Tignan",
+    Cebuano: "Tan-awon",
+    Waray: "Tan-awa",
+    Hiligaynon: "Gapangita",
+  };
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", !isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
 
   useEffect(() => {
     // fetch top searches
@@ -77,19 +122,44 @@ const Index: React.FC = () => {
     window.location.href = `info?${searchQuery}`;
   };
 
+  const toggleDarkMode = () => {
+    if (isDarkMode) {
+      localStorage.setItem("isDarkMode", "false");
+    } else {
+      localStorage.setItem("isDarkMode", "true");
+    }
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar
+          className={isDarkMode === true ? "bg-[#F2F2F2]" : "bg-[#333333]"}
+        >
+          <IonToggle
+            aria-label="Dark toggle"
+            color="dark"
+            onIonChange={() => toggleDarkMode()}
+            checked={!isDarkMode}
+            slot="start"
+            className="toggle-small"
+          />
           <IonTitle>gob.konek</IonTitle>
           <IonSelect
             slot="end"
+            onIonChange={(event) => {
+              setSelectedLanguage(event.detail.value);
+              localStorage.setItem("language", event.detail.value);
+            }}
             value={selectedLanguage}
             className="text-xs"
             interface="popover"
           >
             {languageOptions.map((l) => (
-              <IonSelectOption value={l}>{l}</IonSelectOption>
+              <IonSelectOption key={l} value={l}>
+                {l}
+              </IonSelectOption>
             ))}
           </IonSelect>
         </IonToolbar>
@@ -107,27 +177,33 @@ const Index: React.FC = () => {
                 type="text"
                 onChange={(e) => setSearch(e.target.value)}
                 className="bg-gray-500 bg-opacity-40 rounded-2xl text-md py-3 indent-4 w-full"
-                placeholder="Enter text"
+                placeholder={enterTextTranslations[selectedLanguage]}
               />
-              <div className="flex gap-6">
+              <div className="flex gap-4">
                 {/* filters */}
                 <IonSelect
-                  className="text-xs"
+                  className="text-xs border-b-2"
                   label={selectedProvince !== undefined ? "" : "Province"}
+                  value={selectedProvince ?? undefined}
                   interface="popover"
                   onIonChange={(event) => {
                     setSelectedProvince(event.detail.value);
+                    setSelectedMunicipalityOrProvince(undefined);
+                    localStorage.setItem("province", event.detail.value);
                   }}
                 >
                   {provinces.map((p) => (
-                    <IonSelectOption value={p.key}>{p.name}</IonSelectOption>
+                    <IonSelectOption key={p.key} value={p.key}>
+                      {p.name}
+                    </IonSelectOption>
                   ))}
                 </IonSelect>
                 <IonSelect
-                  className="text-xs"
+                  className="text-xs border-b-2"
                   label={selectedMunicipalityOrCity !== undefined ? "" : "City"}
                   onIonChange={(event) => {
                     setSelectedMunicipalityOrProvince(event.detail.value);
+                    localStorage.setItem("city", event.detail.value);
                   }}
                   interface="popover"
                 >
@@ -137,7 +213,7 @@ const Index: React.FC = () => {
                     cities
                       .filter((c) => c.province === selectedProvince)
                       .map((c) => (
-                        <IonSelectOption value={c.city}>
+                        <IonSelectOption key={cities.indexOf(c)} value={c.city}>
                           {c.name}
                         </IonSelectOption>
                       ))
@@ -148,24 +224,29 @@ const Index: React.FC = () => {
             </div>
             <button
               type="submit"
-              className="bg-blue-500 py-3 px-4 rounded-xl w-[30%] "
+              className="bg-[#A14DB6] text-white py-3 px-4 rounded-xl w-[30%] "
             >
               Search
             </button>
           </form>
           <div className="w-[80vw] md:w-[55vw] lg:w-[40vw] xl:w-[25vw] 2xl:w-[20vw]">
             {/* Popular searches */}
-            <h2 className="font-semibold mb-2">Popular Searches</h2>
+            <h2 className="font-semibold mb-2">
+              {popularSearchesTranslations[selectedLanguage]}
+            </h2>
             <div>
               {/* iterate through list of popular searches */}
               {topSearches.map((s) => {
                 return (
-                  <div className="bg-gray-500 bg-opacity-40 flex justify-between items-center mb-2 py-1.5 px-3 rounded-lg">
+                  <div
+                    key={topSearches.indexOf(s)}
+                    className="border-b-2 border-[#A14DB6] flex justify-between items-center mb-2 py-1.5 px-3 rounded-t-lg"
+                  >
                     <p>
                       {topSearches.indexOf(s) + 1}. {s}
                     </p>
                     <a href={`/info?s=${s}`} className="opacity-30 block">
-                      View
+                      {viewTranslations[selectedLanguage]}
                     </a>
                   </div>
                 );
--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -6,21 +6,35 @@ import {
   IonItem,
   IonLabel,
   IonPage,
+  IonSelect,
+  IonSelectOption,
   IonTitle,
+  IonToggle,
   IonToolbar,
 } from "@ionic/react";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+interface Translation {
+  English: string;
+  Filipino: string;
+  Cebuano: string;
+  Waray: string;
+  Hiligaynon: string;
+}
 
 interface Career {
   title: string;
   duration: string;
   description: string;
+  link: string;
 }
 
 interface PoliticalRelative {
   name: string;
   relation: string;
   currentPosition: string;
+  link: string;
 }
 
 interface Case {
@@ -35,6 +49,7 @@ interface Legislation {
   status: string;
   description: string;
   dateFiled: Date;
+  link: string;
 }
 
 // HS diploma, vocational, bachelor's degree, etc.
@@ -42,6 +57,7 @@ interface Education {
   attained: string;
   school: string;
   yearCompleted: string;
+  link: string;
 }
 
 interface Project {
@@ -49,6 +65,7 @@ interface Project {
   duration: string;
   description: string;
   status: string;
+  link: string;
 }
 
 interface Summary {
@@ -63,16 +80,57 @@ interface Summary {
   projects: Project[];
 }
 
+interface LinkWrapper {
+  link: string;
+}
+
+const backTranslation: Translation = {
+  English: "Go Back",
+  Filipino: "Mag-search Ulit",
+  Cebuano: "Mag-search Liwat",
+  Waray: "Mag-search Utro",
+  Hiligaynon: "Mag-search Liwat",
+};
+
+const SourceComponent: React.FC<LinkWrapper> = ({ link }) => {
+  return (
+    <p>
+      <a
+        onClick={() => window.location.replace(link)}
+        className="flex items-end gap-1.5"
+      >
+        <span className="block h-4">Source</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="size-4"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+          />
+        </svg>
+      </a>
+    </p>
+  );
+};
+
 const CareerComponent: React.FC<Career> = ({
   title,
   duration,
   description,
+  link,
 }) => {
   return (
     <div>
       <h3>{title}</h3>
       <p className="opacity-50">{duration}</p>
       <p>{description}</p>
+      <SourceComponent link={link} />
     </div>
   );
 };
@@ -81,12 +139,14 @@ const PoliticalDynastyComponent: React.FC<PoliticalRelative> = ({
   name,
   relation,
   currentPosition,
+  link,
 }) => {
   return (
     <div>
       <h3>{name}</h3>
       <p className="opacity-50">{relation}</p>
       <p>{currentPosition}</p>
+      <SourceComponent link={link} />
     </div>
   );
 };
@@ -96,6 +156,7 @@ const LegislationComponent: React.FC<Legislation> = ({
   status,
   description,
   dateFiled,
+  link,
 }) => {
   return (
     <div>
@@ -103,6 +164,7 @@ const LegislationComponent: React.FC<Legislation> = ({
       <p className="opacity-50">{status}</p>
       <p>{description}</p>
       <p>Filed on ${dateFiled.toString()}</p>
+      <SourceComponent link={link} />
     </div>
   );
 };
@@ -118,7 +180,7 @@ const CaseComponent: React.FC<Case> = ({
       <h3>{title}</h3>
       <p className="opacity-50">{description}</p>
       <p>Filed on ${dateFiled.toString()}</p>
-      <p>Source: ${link}</p>
+      <SourceComponent link={link} />
     </div>
   );
 };
@@ -128,6 +190,7 @@ const ProjectComponent: React.FC<Project> = ({
   duration,
   description,
   status,
+  link,
 }) => {
   return (
     <div>
@@ -135,6 +198,7 @@ const ProjectComponent: React.FC<Project> = ({
       <p className="opacity-50">{status}</p>
       <p>{description}</p>
       <p>{duration}</p>
+      <SourceComponent link={link} />
     </div>
   );
 };
@@ -143,133 +207,217 @@ const EducationComponent: React.FC<Education> = ({
   attained,
   school,
   yearCompleted,
+  link,
 }) => {
   return (
     <div>
       <h3>{attained}</h3>
       <p className="opacity-50">{school}</p>
       <p>Completed {yearCompleted}</p>
+      <SourceComponent link={link} />
     </div>
   );
 };
 
 const Info: React.FC = () => {
   const [summary, setSummary] = useState<Summary | undefined>();
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(
+    localStorage.getItem("isDarkMode") === "false" ? false : true,
+  );
+  const [selectedLanguage, setSelectedLanguage] = useState<string>(
+    localStorage.getItem("language") ?? "English",
+  );
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const name = queryParams.get("s");
+  const province = queryParams.get("p");
+  const city = queryParams.get("m");
+  const language = queryParams.get("l");
+  const languageOptions: string[] = [
+    "English",
+    "Filipino",
+    "Cebuano",
+    "Waray",
+    "Hiligaynon",
+  ];
+  const apiUrl: string = "https://lean-tech-api.vercel.app/retrieve/summary";
 
   useEffect(() => {
-    setSummary({
-      commonName: "Juan dela Cruz",
-      legalName: "Juan Carlos dela Cruz",
-      description:
-        "A prominent leader in local politics dedicated to community development and environmental sustainability.",
-      careers: [
-        {
-          title: "Mayor of San Isidro",
-          duration: "2016 - Present",
-          description:
-            "Elected to lead the city, focusing on economic growth and community initiatives.",
-        },
-        {
-          title: "City Councilor of San Isidro",
-          duration: "2010 - 2016",
-          description:
-            "Developed policies and legislation aimed at improving local infrastructure and public services.",
-        },
-      ],
-      dynasty: [
-        {
-          name: "Maria dela Cruz",
-          relation: "Mother",
-          currentPosition: "Former Governor of San Isidro",
-        },
-        {
-          name: "Jose dela Cruz",
-          relation: "Uncle",
-          currentPosition: "Former Senator",
-        },
-      ],
-      legislations: [
-        {
-          title: "San Isidro Urban Development Act",
-          status: "Passed",
-          description:
-            "Legislation aimed at enhancing urban infrastructure and housing projects in San Isidro.",
-          dateFiled: new Date("2022-03-15"),
-        },
-        {
-          title: "Public Health Improvement Bill",
-          status: "Pending",
-          description:
-            "A bill focused on increasing funding for healthcare facilities in the district.",
-          dateFiled: new Date("2023-01-10"),
-        },
-      ],
-      education: [
-        {
-          attained: "Bachelor's Degree",
-          school: "University of San Isidro",
-          yearCompleted: "2005-04-15",
-        },
-        {
-          attained: "High School Diploma",
-          school: "San Isidro High School",
-          yearCompleted: "2001-05-30",
-        },
-      ],
-      cases: [
-        {
-          title: "Corruption Allegations",
+    document.body.setAttribute("data-theme", !isDarkMode ? "dark" : "light");
+  }, [isDarkMode]);
 
-          description:
-            "Investigation into alleged misuse of public funds during the mayoral campaign.",
+  useEffect(() => {
+    console.log([name, province, city, language]);
 
-          dateFiled: "2022-05-01",
+    if (!province) {
+      fetch(apiUrl.concat(`?name=${name}`), {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
 
-          link: "https://example.com/cases/corruption-allegations",
-        },
+    // retrieve from API
+    // setSummary({
+    //   commonName: "Juan dela Cruz",
+    //   legalName: "Juan Carlos dela Cruz",
+    //   description:
+    //     "A prominent leader in local politics dedicated to community development and environmental sustainability.",
+    //   careers: [
+    //     {
+    //       title: "Mayor of San Isidro",
+    //       duration: "2016 - Present",
+    //       description:
+    //         "Elected to lead the city, focusing on economic growth and community initiatives.",
+    //       link: "www.example.com",
+    //     },
+    //     {
+    //       title: "City Councilor of San Isidro",
+    //       duration: "2010 - 2016",
+    //       description:
+    //         "Developed policies and legislation aimed at improving local infrastructure and public services.",
+    //       link: "www.example.com",
+    //     },
+    //   ],
+    //   dynasty: [
+    //     {
+    //       name: "Maria dela Cruz",
+    //       relation: "Mother",
+    //       currentPosition: "Former Governor of San Isidro",
+    //       link: "www.example.com",
+    //     },
+    //     {
+    //       name: "Jose dela Cruz",
+    //       relation: "Uncle",
+    //       currentPosition: "Former Senator",
+    //       link: "www.example.com",
+    //     },
+    //   ],
+    //   legislations: [
+    //     {
+    //       title: "San Isidro Urban Development Act",
+    //       status: "Passed",
+    //       description:
+    //         "Legislation aimed at enhancing urban infrastructure and housing projects in San Isidro.",
+    //       dateFiled: new Date("2022-03-15"),
+    //       link: "www.example.com",
+    //     },
+    //     {
+    //       title: "Public Health Improvement Bill",
+    //       status: "Pending",
+    //       description:
+    //         "A bill focused on increasing funding for healthcare facilities in the district.",
+    //       dateFiled: new Date("2023-01-10"),
+    //       link: "www.example.com",
+    //     },
+    //   ],
+    //   education: [
+    //     {
+    //       attained: "Bachelor's Degree",
+    //       school: "University of San Isidro",
+    //       yearCompleted: "2005-04-15",
+    //       link: "www.example.com",
+    //     },
+    //     {
+    //       attained: "High School Diploma",
+    //       school: "San Isidro High School",
+    //       yearCompleted: "2001-05-30",
+    //       link: "www.example.com",
+    //     },
+    //   ],
+    //   cases: [
+    //     {
+    //       title: "Corruption Allegations",
 
-        {
-          title: "Environmental Violation Case",
+    //       description:
+    //         "Investigation into alleged misuse of public funds during the mayoral campaign.",
 
-          description:
-            "Case filed against the city for failing to comply with environmental regulations.",
+    //       dateFiled: "2022-05-01",
 
-          dateFiled: "2023-02-20",
+    //       link: "https://example.com/cases/corruption-allegations",
+    //     },
 
-          link: "https://example.com/cases/environmental-violation",
-        },
-      ],
-      projects: [
-        {
-          title: "Community Clean-Up Initiative",
-          duration: "2021 - Present",
-          description:
-            "A project aimed at improving cleanliness and environmental awareness in San Isidro.",
-          status: "Ongoing",
-        },
-        {
-          title: "Youth Empowerment Program",
-          duration: "2018 - 2020",
-          description:
-            "A program designed to provide skills training and leadership opportunities for the youth in the community.",
-          status: "Completed",
-        },
-      ],
-    });
+    //     {
+    //       title: "Environmental Violation Case",
+
+    //       description:
+    //         "Case filed against the city for failing to comply with environmental regulations.",
+
+    //       dateFiled: "2023-02-20",
+
+    //       link: "https://example.com/cases/environmental-violation",
+    //     },
+    //   ],
+    //   projects: [
+    //     {
+    //       title: "Community Clean-Up Initiative",
+    //       duration: "2021 - Present",
+    //       description:
+    //         "A project aimed at improving cleanliness and environmental awareness in San Isidro.",
+    //       status: "Ongoing",
+    //       link: "www.example.com",
+    //     },
+    //     {
+    //       title: "Youth Empowerment Program",
+    //       duration: "2018 - 2020",
+    //       description:
+    //         "A program designed to provide skills training and leadership opportunities for the youth in the community.",
+    //       status: "Completed",
+    //       link: "www.example.com",
+    //     },
+    //   ],
+    // });
   }, []);
+
+  const toggleDarkMode = () => {
+    if (isDarkMode) {
+      localStorage.setItem("isDarkMode", "false");
+    } else {
+      localStorage.setItem("isDarkMode", "true");
+    }
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar
+          className={isDarkMode === true ? "bg-[#F2F2F2]" : "bg-[#333333]"}
+        >
+          <IonToggle
+            aria-label="Dark toggle"
+            color="dark"
+            onIonChange={() => toggleDarkMode()}
+            checked={!isDarkMode}
+            slot="start"
+            className="toggle-small"
+          />
           <IonTitle>gob.konek</IonTitle>
+          <IonSelect
+            slot="end"
+            onIonChange={(event) => {
+              setSelectedLanguage(event.detail.value);
+              localStorage.setItem("language", event.detail.value);
+            }}
+            value={selectedLanguage}
+            className="text-xs"
+            interface="popover"
+          >
+            {languageOptions.map((l) => (
+              <IonSelectOption key={l} value={l}>
+                {l}
+              </IonSelectOption>
+            ))}
+          </IonSelect>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <div className="flex flex-col justify-center items-center text-sm py-10">
           <div className="w-[80vw] md:w-[55vw] lg:w-[40vw] xl:w-[25vw] 2xl:w-[20vw] mb-3">
             <a href="/" className="text-blue-500 text-lg">
-              Go Back
+              {backTranslation[selectedLanguage]}
             </a>
           </div>
           <div className="w-[80vw] md:w-[55vw] lg:w-[40vw] xl:w-[25vw] 2xl:w-[20vw] mb-4">
@@ -294,6 +442,7 @@ const Info: React.FC = () => {
                       title={c.title}
                       duration={c.duration}
                       description={c.description}
+                      link={c.link}
                     />
                   ))}
                 </div>
@@ -314,6 +463,7 @@ const Info: React.FC = () => {
                       name={d.name}
                       relation={d.relation}
                       currentPosition={d.currentPosition}
+                      link={d.link}
                     />
                   ))}
                 </div>
@@ -335,6 +485,7 @@ const Info: React.FC = () => {
                       status={l.status}
                       description={l.description}
                       dateFiled={l.dateFiled}
+                      link={l.link}
                     />
                   ))}
                 </div>
@@ -356,6 +507,7 @@ const Info: React.FC = () => {
                       status={p.status}
                       description={p.description}
                       duration={p.duration}
+                      link={p.link}
                     />
                   ))}
                 </div>
@@ -397,6 +549,7 @@ const Info: React.FC = () => {
                       attained={s.attained}
                       yearCompleted={s.yearCompleted}
                       school={s.school}
+                      link={s.link}
                     />
                   ))}
                 </div>
